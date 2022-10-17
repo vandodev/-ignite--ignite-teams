f81efffd183dc4d1b51b42289e946072a101fc13
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-17 18:31:32 -0300

    :fire: :rocket: :fire: Alterando estilização para styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,23 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
+import {Container, Title} from './styles'
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Groups</Text>
-    </View>
+    <Container>
+      <Title >Groups</Title>
+    </Container>    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color:"#fff",
-    fontSize:32
-
-  }
-});
