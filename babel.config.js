f0684c80249cb22be21b93c0c8986770ad4571cd
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-17 18:49:17 -0300

    :fire: :rocket: :fire: Criando theme da aplicação

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,6 @@ module.exports = function(api) {
             '@components': './src/components',
             '@screens': './src/screens',
             '@storage': './src/storage',
-            '@theme': './src/theme',
             '@utils': './src/utils',
           }
         }
