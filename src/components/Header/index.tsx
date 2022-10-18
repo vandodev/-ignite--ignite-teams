fc48bad2497e577adb5d31f4baf2c0ed64c87101
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-18 17:52:15 -0300

    :fire: :rocket: :fire: Trabalhando com phosphoricons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackIcon } from "./styles";
 import logoImg from '@assets/logo.png';
+
 
 export function Header() {
 
   return (
     <Container>
+      {/* <CaretLeft color="#fff" size={32}/> */}
+      <BackIcon />
       <Logo source={logoImg} />
     </Container>
   )
