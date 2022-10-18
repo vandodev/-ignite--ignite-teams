611d9fbcde46314728c7ba6138723ad92b371bf7
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-18 17:19:34 -0300

    :fire: :rocket: :fire: Fonts personalizadas

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
+import {ActivityIndicator} from "react-native"
 import { ThemeProvider } from 'styled-components/native'; 
 import { Groups } from '@screens/Groups';
 import theme from './src/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator/>}
     </ThemeProvider>
     
   );
