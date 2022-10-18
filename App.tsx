7eabcc5532476feea4f6e6d2c54bf91000ce86f5
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-18 17:33:03 -0300

    :fire: :rocket: :fire: Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'; 
 import { Groups } from '@screens/Groups';
 import theme from './src/theme';
@@ -10,7 +11,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <Loading/>}
+      <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      {fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
     
   );
