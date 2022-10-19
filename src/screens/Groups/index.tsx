a0d344274bacad420fee4c62c75eaef5d05470ff
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-19 18:39:30 -0300

    :fire: :rocket: :fire: Criando component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import {Container, } from './styles'
@@ -9,6 +10,10 @@ export function Groups() {
       <Highlight
         title='Turmas'
         subtitle='Jogue com a sua turma'
+      />
+
+      <GroupCard
+        title='Galera do ignite'
       />
      
     </Container>    
