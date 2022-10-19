f1a7574210e7c856468c6358c06d12a182b340ce
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-19 18:25:24 -0300

    :fire: :rocket: :fire:Criando component Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import {Container, } from './styles'
 
 export function Groups() {
   return (
     <Container>
-      <Header showBackButton/>
+      <Header/>
+      <Highlight
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
      
     </Container>    
   );
