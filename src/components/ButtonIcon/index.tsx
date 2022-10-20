8adcc994c255f0517d1910c28fda56d3b0f230eb
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-20 17:17:16 -0300

    :fire: :rocket: :fire: Criando component ButtonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,13 @@
+import { Container,Icon } from './styles';
+
+export function ButtonIcon() {
+  return(
+    <Container >
+      <Icon 
+        name="home"
+        type="PRIMARY"
+      />
+   
+    </Container>
+  );
+}
