f9dc1305446800e5318dba5424814d95790abcc3
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-20 17:01:11 -0300

    :fire: :rocket: :fire: Estrutura da tela Players

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from 'styled-components/native';
 
 import { NewGroup} from '@screens/NewGroup';
 import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
@@ -18,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-      {fontsLoaded ? <NewGroup /> : <Loading/>}
+      {fontsLoaded ? <Players /> : <Loading/>}
     </ThemeProvider>
     
   );
