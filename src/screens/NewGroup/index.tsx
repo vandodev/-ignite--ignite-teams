0f57167a8032666ea703d7ebde52b59baf68677f
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-20 16:20:44 -0300

    :fire: :rocket: :fire: Criando component Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 import { Container,Content, Icon} from "./styles";
 
 export function NewGroup() {
@@ -15,7 +16,7 @@ export function NewGroup() {
                     subtitle="crie a turma para adicionar as pessoas"
                 />
 
-             
+                <Input />
 
                 <Button 
                     title="Criar"
