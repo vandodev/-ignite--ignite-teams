832d77768d6eb8f94c5b86365010d6d1678d8961
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-22 17:46:15 -0300

    :fire: :rocket: :fire: Estrutura de navegação

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'; 
 
-import { NewGroup} from '@screens/NewGroup';
-import { Groups } from '@screens/Groups';
-import { Players } from '@screens/Players';
+import {Routes} from './src/routes';
 
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
@@ -20,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-      {fontsLoaded ? <Players /> : <Loading/>}
+      {fontsLoaded ? <Routes /> : <Loading/>}
     </ThemeProvider>
     
   );
