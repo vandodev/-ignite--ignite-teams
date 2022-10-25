f3f690a2ebac05bdc88115ebe8254dea8148a18b
Evandro oliveira <vandoaparecido@hotmail.com>
2022-10-25 20:21:54 -0300

    :fire: :rocket: :fire: Tratando excecões

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import { useState} from 'react';
+import {Alert} from 'react-native'
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -6,6 +7,7 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Container,Content, Icon} from "./styles";
 import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
 
 export function NewGroup() {
     const [group, setGroup] = useState('');
@@ -13,10 +15,18 @@ export function NewGroup() {
 
     async function handleNew() {
         try{
+            if(group.trim().length === 0) {
+             return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+            }
             await groupCreate(group)
             navigation.navigate('players',{group})
         }catch(error){
-            console.log(error)
+               if(error instanceof AppError) {
+                    Alert.alert('Novo Grupo', error.message);
+                } else{
+                    Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+                    console.log(error);
+                }
         }
     }
 
